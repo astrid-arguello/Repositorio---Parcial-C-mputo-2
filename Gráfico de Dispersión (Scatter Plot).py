53c56f1dbcb4316d0ee3bacd34e3520e6d73bd85
Karen Jnadres <karenjandres470@gmail.com>
2025-10-18 23:31:27 -0600

    Update Gráfico de Dispersión (Scatter Plot).py

--- Gráfico de Dispersión (Scatter Plot).py
+++ Gráfico de Dispersión (Scatter Plot).py
@@ -1,11 +1,18 @@
+# Importancion de la libreria
 import plotly.express as px
 import pandas as pd
 
+# En estas dos lineas de codigo es donde van los datos de entrada 
+# que llevara el grafico de dispercion.
 software = ['Procesadores y hojas de cálculo', 'Programas especializados', 'Plataformas de videoconferencia', 'Apps de notas']
 respuestas = [2, 2, 4, 3]
 
+# Esta linea de comandos es donde se estructuran los datos para
+# general el grafico de dispercion.
 df = pd.DataFrame({'Software': software, 'Respuestas': respuestas})
 
+# Estas lineas de comando son las escargadas de
+# Crear y mostrar el grafico de dispercion 
 fig = px.scatter(df,
                  x='Software',
                  y='Respuestas',
